382396843016d97d7be758df193df63f68a96fdd
Brent Schooley <schooleybrent@gmail.com>
2016-10-22 12:33:54 -0400

    Philly Code Camp updates

--- SwiftIntroToSwift.playground/Pages/Swift 3.0 Miscellaneous.xcplaygroundpage/Contents.swift
+++ SwiftIntroToSwift.playground/Pages/Swift 3.0 Miscellaneous.xcplaygroundpage/Contents.swift
@@ -11,7 +11,7 @@ var names = ["Tom", "Sally", "Sue", "John"]
 //This #if swift(>=3.0) is new in Swift 3.0
 #if swift(>=3.0)
     // All function parameters have labels unless you request otherwise
-    //UIFont.preferredFon
+    UIFont.preferredFont(forTextStyle: .subheadline)
     
     class MyTableViewController: UITableViewController {
         override func numberOfSections(in tableView: UITableView) -> Int {
@@ -19,26 +19,25 @@ var names = ["Tom", "Sally", "Sue", "John"]
         }
     }
     // Omit needless words
+    UIFont.preferredFont(forTextStyle: .subheadline)
     
     let fullname = name.appending(" Schooley")
-    // Upper camel case replaced with lower camel case for properties and enum values
+  
+    let blue = UIColor.blue
+    let min = numbers.min()
+    UIDevice.current
     
-    
-    // "Swifty" importing of C functions
-    // (start with if let ctx = UIGraphicsGetCurrentContext() {)
-    
-    
-if let ctx = UIGraphicsGetCurrentContext() {
-    ctx.setFillColor(UIColor.red.cgColor)
-}
-    
-    
- 
+    let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+    if let ctx = UIGraphicsGetCurrentContext() {
+      ctx.setFillColor(UIColor.red.cgColor)
+      ctx.setStrokeColor(UIColor.black.cgColor)
+      ctx.setLineWidth(10)
+      ctx.addRect(rectangle)
+      ctx.drawPath(using: .fillStroke)
+    }
 #else
 
     // All function parameters have labels unless you request otherwise
-    UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
-    
     
     class MyTableViewController: UITableViewController {
         override func numberOfSectionsInTableView(tableView: UITableView) {
@@ -48,10 +47,12 @@ if let ctx = UIGraphicsGetCurrentContext() {
     
     
     // Omit needless words
+  UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+  
+  let fullName = name.stringByAppendingString(" Schooley")
+  
     let blue = UIColor.blueColor()
     let min = numbers.minElement()
-    attributedString.appendAttributedString(anotherString)
-    names.insert("Jane", atIndex: 0)
     UIDevice.currentDevice()
     
     
